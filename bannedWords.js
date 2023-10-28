2b32a4bbf13ff419cf9d0be4e5aa7d70462f1dad
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2023-10-28 16:26:25 +0200

    fix(oldstyle): works in old style FC

--- bannedWords.js
+++ bannedWords.js
@@ -12,21 +12,34 @@ function restoreOptions() {
 
 function cleanPage(bannedWords) {
     const currentPath = window.location.pathname;
-    const forumPath = "/foro/forumdisplay.php"
-    const trendingPath = "/foro/trending.php"
+    console.debug("current path ", currentPath);
+    const homePath = "/";
+    const forumPath = "/foro/forumdisplay.php";
+    const trendingPath = "/foro/trending.php";
+
     const forumTopicCSSRoute = "section.without-top-corners.without-bottom-corners > div";
+    const oldForumTopicSSRoute = '#threadslist tbody[id^="collapseobj_st_"]>tr, #threadslist tbody[id^="threadbits_forum_"]>tr';
+    const homeCSSRoute = ".cajasnews>tbody>tr:nth-child(3)>td.texto>table>tbody>tr";
     const trendingTopicCSSRoute = forumTopicCSSRoute;
 
 
-    let query;
-    if (currentPath === trendingPath) {
-        query = trendingTopicCSSRoute;
-    } else if (currentPath === forumPath) {
-        query = forumTopicCSSRoute;
+    isOldStyle = document.getElementById("inlinemodform") != null
+
+    const pageQuery = {
+        [forumPath]: isOldStyle ? oldForumTopicSSRoute : forumTopicCSSRoute,
+        [trendingPath]: trendingTopicCSSRoute,
+        [homePath]: homeCSSRoute
+    }
+
+
+    let query = pageQuery[currentPath];
+    if (query == null) {
+        console.debug("no query found for current page", pageQuery);
+        return;
     }
 
     let childs = document.querySelectorAll(query);
-    console.debug("childs to check", childs);
+    console.debug("childs to check", childs, "with query", query);
 
     let len = childs.length;
     let removed = 0
@@ -35,12 +48,12 @@ function cleanPage(bannedWords) {
         let content = c.textContent;
         for (let bw of bannedWords) {
             if (content.toUpperCase().includes(bw.toUpperCase())) {
-                console.debug("child", c, " removed because it contains ", bw);
+                console.debug("child", c, "removed because it contains", bw);
                 removed++;
-                c.remove()
+                c.remove();
                 break;
             }
         }
     }
-    console.debug("bannedWordsFC end, removed elements: ", removed);
+    console.debug("bannedWordsFC execution ended. It removed", removed, "elements");
 }
